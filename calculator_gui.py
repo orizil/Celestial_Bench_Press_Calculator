cf06c85def80436bc81237eb0aea9b19331caab6
orizil <105450855+orizil@users.noreply.github.com>
2023-07-05 22:17:57 +0300

    Added info

--- calculator_gui.py
+++ calculator_gui.py
@@ -16,13 +16,14 @@ class CelestialBenchPressCalculator:
         self.right_panel = ctk.CTkFrame(self.root)
         self.right_panel.grid(row=0, column=1, padx=5, pady=5)
 
+        self.weight = tk.StringVar()
+        self.weight.set('50')
+
         self.create_planet_buttons()
         self.create_input_label()
         self.create_result_label()
         self.create_info_label()
-        self.create_weight_entry()
-        self.create_slider()
-
+        self.create_slider_and_entry()
         self.weight.trace_add("write", self.update_slider_from_entry)
 
     def create_planet_buttons(self):
@@ -33,9 +34,8 @@ class CelestialBenchPressCalculator:
             button_row = button_index // 2
             button_column = button_index % 2
 
-            planet_button = PlanetButton(self.left_panel, image=image, text=name.capitalize(),
-                                         fg_color="transparent", calculator=self,
-                                         planet_name=name)
+            planet_button = ctk.CTkButton(self.left_panel, image=image, text=name.capitalize(),
+                                          fg_color="transparent", command=lambda planet=name: self.button_click(planet))
             planet_button.grid(row=button_row, column=button_column, pady=5)
 
             button_index += 1
@@ -43,35 +43,29 @@ class CelestialBenchPressCalculator:
     def create_input_label(self):
         label_text = "Current max bench press (kg):"
         label = ctk.CTkLabel(self.right_panel, text=label_text, font=("Josefin", 18))
-        label.grid(row=0, column=0, columnspan=2, padx=15, pady=(15, 5))
-
-    def create_weight_entry(self):
-        self.weight = tk.StringVar()
-        self.weight.set('50')
-
-        self.bench_press_entry = WeightEntry(self.right_panel, textvariable=self.weight, width=40,
-                                             calculator=self)
-        validate_cmd = self.right_panel.register(self.validate_input)
-        self.bench_press_entry.configure(validate="key", validatecommand=(validate_cmd, '%P'))
-        self.bench_press_entry.grid(row=1, column=0, padx=15)
-
-    def create_slider(self):
-        self.bench_press_slider = ctk.CTkSlider(self.right_panel, from_=0, to=300)
-        self.bench_press_slider.set(50)
-        self.bench_press_slider.bind("<B1-Motion>", self.update_entry_from_slider)
-        self.bench_press_slider.bind("<ButtonRelease-1>", self.update_entry_from_slider)
-        self.bench_press_slider.grid(row=1, column=1, padx=15, pady=(5, 15))
-
-    def create_result_label(self):
-        self.result_label = ResultLabel(self.right_panel, text="Select a celestial object",
-                                        font=("Josefin", 14))
-        self.result_label.grid(row=2, column=0, columnspan=2, padx=15, pady=(5, 15))
+        label.grid(row=1, column=0, columnspan=2, padx=15, pady=(15, 5))
 
     def create_info_label(self):
-        self.info_label = ResultLabel(self.right_panel, text="Select a celestial object",
-                                        font=("Josefin", 14))
-        self.result_label.grid(row=2, column=0, columnspan=2, padx=15, pady=(5, 15))
+        label_text = "Welcome to the Celestial Bench Press Calculator!\n\n  Choose a planet from the solar system,\n" \
+                     "the moon, or 433-Eros - the first soft-landed asteroid!"
+        label = ctk.CTkLabel(self.right_panel, text_color="gray", text=label_text, font=("Josefin", 12))
+        label.grid(row=0, column=0, columnspan=2, padx=15, pady=15)
 
+    def create_slider_and_entry(self):
+        self.bench_press_entry = ctk.CTkEntry(self.right_panel, textvariable=self.weight, width=40)
+        validate_cmd = self.right_panel.register(self.validate_input)
+        self.bench_press_entry.configure(validate="key", validatecommand=(validate_cmd, '%P'))
+        self.bench_press_entry.grid(row=2, column=0, padx=15)
+
+        self.bench_press_slider = ctk.CTkSlider(self.right_panel, from_=0, to=300)
+        self.bench_press_slider.set(50)
+        self.bench_press_slider.grid(row=2, column=1, padx=15, pady=(5, 15))
+        self.bench_press_slider.bind("<B1-Motion>", self.update_entry_from_slider)
+        self.bench_press_slider.bind("<ButtonRelease-1>", self.update_entry_from_slider)
+
+    def create_result_label(self):
+        self.result_label = ctk.CTkLabel(self.right_panel, text="Select a celestial object", font=("Josefin", 14))
+        self.result_label.grid(row=3, column=0, columnspan=2, padx=15, pady=(5, 15))
 
     def button_click(self, planet):
         self.selected_planet.set(planet)
@@ -88,7 +82,7 @@ class CelestialBenchPressCalculator:
             self.bench_press_slider.set(slider_bench_press)
             self.calc_result()
         except ValueError:
-            pass
+            self.result_label.configure(text="Invalid input")
 
     def validate_input(self, val):
         if val.isdigit():
@@ -102,47 +96,11 @@ class CelestialBenchPressCalculator:
         self.result_label.configure(text=f"Your bench press on {self.selected_planet.get().capitalize()}: {result} kg")
 
 
-class PlanetButton(ctk.CTkButton):
-    def __init__(self, master, **kwargs):
-        self.calculator = kwargs.pop('calculator')
-        self.planet_name = kwargs.pop('planet_name')
-        image = kwargs.pop('image')
-
-        super().__init__(master, image=image, **kwargs)
-
-        self.configure(command=self.button_click)
-
-    def button_click(self):
-        self.calculator.button_click(self.planet_name)
-
-
-class WeightEntry(ctk.CTkEntry):
-    def __init__(self, master, **kwargs):
-        self.calculator = kwargs.pop('calculator')
-
-        super().__init__(master, **kwargs)
-
-    def insert(self, index, string):
-        super().insert(index, string)
-        self.calculator.update_slider_from_entry()
-
-    def delete(self, start, end=None):
-        super().delete(start, end)
-        self.calculator.update_slider_from_entry()
-
-
-class ResultLabel(ctk.CTkLabel):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-
-
 if __name__ == '__main__':
     ctk.set_appearance_mode('dark')
     root = ctk.CTk()
     root.title("Celestial Bench Press Calculator")
     root.geometry('600x400')
-    root.grid_columnconfigure(0, weight=1)
-    root.grid_columnconfigure(1, weight=1)
 
     calculator = CelestialBenchPressCalculator(root)
 
